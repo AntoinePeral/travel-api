8b6a170c9297462bc63a5d9728733670ead87047
antop <antoine.peral@rossel.fr>
2025-08-07 22:36:09 +0200

    push redeploy app 2 with traeffik on back wirth log

--- src/main/java/com/wcs/travel_blog/exception/GlobalExceptionHandler.java
+++ src/main/java/com/wcs/travel_blog/exception/GlobalExceptionHandler.java
@@ -6,6 +6,8 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
